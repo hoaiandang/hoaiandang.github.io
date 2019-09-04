8fa3b74ff01f4b818f2c89767c4340b87bf43a72
Hoaian Dang <hoaiandang@berkeley.edu>
2019-09-03 21:01:05 -0700

    fix overflow hidden error after update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,35 @@
 $(document).ready(function() {
-    
+
     function circleShow() {
         // jQuery Function Number 3
-        $("#top").fadeIn(50);
-        
-        setTimeout(circleOut, 50);
+        $("#top").fadeIn(1000);
+        setTimeout(introClean, 320);
+
+        //setTimeout(circleOut, 50);
     }
-    
+
     function circleOut() {
         // jQuery Function Number 1
         // jQuery Function Number 2
         $("#container").removeClass("closed").addClass("opened");
-        setTimeout(introClean, 900)
-    }    
-    
+        //setTimeout(introClean, 900)
+    }
+
+    function introClean2() {
+        $("#intro").hide();
+    }
+
     function introClean() {
         // jQuery Function Number 4
-        $("#intro").hide();
         // jQuery Function Number 5
         $("#container").css("transition-duration", "0s");
-        
         $("#top").removeClass("container-container");
         $("#container").removeClass("opened").addClass("full");
         $("#container").css("overflow", "initial");
-        
+        setTimeout(introClean2, 680);
         setTimeout(openNav, 0);
     }
-    
+
     function switchZIndex() {
         if ($("#black-page").css("z-index") == 0) {
             $("#black-page").css("z-index", 1);
@@ -36,91 +39,97 @@ $(document).ready(function() {
             $("#main-page").css("z-index", 1);
         }
     }
-    
-    var myVar;  
+
+    var myVar;
     function hideNavBar() {
         $("#nav-bar").hide();
     }
-    
+
     function myStopFunction() {
         clearTimeout(myVar);
     }
-    
+
     function closeNav() {
         $("#burger").addClass("burger-inactive").removeClass("burger-active");
         $("#nav-container").addClass("nav-closed").removeClass("nav-opened");
-        myVar = setTimeout(hideNavBar, 600);
+        $("#nav-bar").addClass("nav-bar-closed").removeClass("nav-bar-opened");
+        //myVar = setTimeout(hideNavBar, 600);
+        myStopFunction();
     }
-    
+
     function openNav() {
         $("#nav-bar").show();
         $("#burger").addClass("burger-active").removeClass("burger-inactive");
         $("#nav-container").addClass("nav-opened").removeClass("nav-closed");
-        myStopFunction();
+        myVar = setTimeout(openBar,250)
     }
-    
+
+    function openBar() {
+      $("#nav-bar").addClass("nav-bar-opened").removeClass("nav-bar-closed");
+    }
+
     function playMessages() {
         console.log("1");
         setTimeout(playMessage1, 1000)
     }
-    
+
     function playMessage1() {
         console.log("2");
         $("#message-1").css("opacity", ".8");
         setTimeout(fadeMessage1, 3200);
     }
-    
+
     function fadeMessage1() {
         console.log("3");
         $("#message-1").css("opacity", "0");
         setTimeout(playMessage2, 1600);
     }
-    
+
     function playMessage2() {
         console.log("4");
         $("#message-2").css("opacity", "0.8");
         setTimeout(fadeMessage2, 4800);
     }
-    
+
     function fadeMessage2() {
         console.log("5");
         $("#message-2").css("opacity", "0");
         setTimeout(playMessage3, 1600);
     }
-    
+
     function playMessage3() {
         console.log("6");
         $("#message-3").css("opacity", "0.8");
         setTimeout(fadeMessage3, 4800);
     }
-    
+
     function fadeMessage3() {
         console.log("7");
         $("#message-3").css("opacity", "0");
         setTimeout(playMessage4, 1600);
     }
-    
+
     function playMessage4() {
         console.log("8");
         $("#message-4").css("opacity", "0.8");
         setTimeout(fadeMessage4, 4200);
     }
-    
+
     function fadeMessage4() {
         console.log("9");
         $("#message-4").css("opacity", "0");
     }
 
-    
-    
-    
+
+
+
     console.log($(".message").attr("message") + $(".message").attr("message"));
     console.log($(".message").html());
     $("#nav-bar").hide();
     $("#top").hide();
     setTimeout(circleShow, 800);
-    
-    
+
+
     $("#bars").click(function() {
         // jQuery Function Number 6
         if ($("#burger").hasClass("burger-active")) {
@@ -129,34 +138,34 @@ $(document).ready(function() {
             openNav();
         }
     })
-    
+
     $(".selected").click(function() {
         if ($("#burger").hasClass("burger-active")) {
             closeNav();
         }
     })
-    
+
     $("#about-page").click(function() {
         console.log($(this).attr("id"));
         if ($("#burger").hasClass("burger-active")) {
             closeNav();
         }
     })
-    
+
     $("#projects-page").click(function() {
         console.log($(this).attr("id"));
         if ($("#burger").hasClass("burger-active")) {
             closeNav();
         }
     })
-    
+
     $("#resume-page").click(function() {
         console.log($(this).attr("id"));
         if ($("#burger").hasClass("burger-active")) {
             closeNav();
         }
     })
-    
+
     $(".hoaian").click(function() {
         if ($("#black-page").hasClass("closed")) {
             $("#black-page").removeClass("closed").addClass("opened");
@@ -171,7 +180,7 @@ $(document).ready(function() {
             console.log("if statement");
         }
     })
-    
+
     $(".nav-element-extra").hover(function(){
         var id = "#" + $(this).attr("id") + "-display";
         var select = $(id);
@@ -193,7 +202,7 @@ $(document).ready(function() {
         $(this.children[0]).css("width", "0%");
         $(this).css("transform", "translateX(0)");
     });
-    
+
     $(".nav-element").hover(function(){
         $(this).css("transform", "translateX(16px)");
         //$(this.children[0]).css("left", "0px");
@@ -204,7 +213,7 @@ $(document).ready(function() {
         $(this.children[0]).css("width", "0%");
         $(this).css("transform", "translateX(0)");
     });
-    
+
     $(".card").hover(function(){
         $(this.children[1]).css("transform", "translateX(0)");
         //$(this.children[0]).css("left", "0px");
@@ -218,16 +227,16 @@ $(document).ready(function() {
         $(this.children[1].children[0]).css("opacity", "0");
         $(this.children[1]).css("transform", "translateX(0)");
     });
-    
-    
+
+
     $(".social").hover(function(){
         $(this.children[0]).css("opacity", "1");
     }, function(){
         $(this.children[0]).css("opacity", "0");
     });
-    
-    
-    
+
+
+
     $(".nav-element").click(function(){
         var thisID = $(this).attr("id");
         console.log(thisID);
@@ -238,9 +247,9 @@ $(document).ready(function() {
         closeNav();
         }
     })
-    
+
     $(".coming-soon").click(function(){
         alert("Coming soon!");
     })
-    
+
 });
